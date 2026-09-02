dc198dc3b7e735f2ff21bef3138f4ae1abc33841
najung <ngh2104@naver.com>
2026-09-02 15:59:08 +0900

    chore: application 파일 추가

--- src/main/java/com/njung/moneyflow/MoneyFlowApplication.java
+++ src/main/java/com/njung/moneyflow/MoneyFlowApplication.java
@@ -9,8 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MoneyFlowApplication {
 
     public static void main(String[] args) {
-        log.info(String.valueOf(Math.PI));
-//        SpringApplication.run(MoneyFlowApplication.class, args);
+        SpringApplication.run(MoneyFlowApplication.class, args);
     }
 
 }
